7d3e3d991671cfe9e470ca5f9cd863bdd9e2c69e
Wendelcorp <bryce@brycewendelaar.com>
2017-06-04 20:50:01 -0400

    Updated apifetch

--- apifetch.js
+++ apifetch.js
@@ -1,8 +1,25 @@
+// To use in parent file:
 // import {fetchWeather} from './apifetch'
+// componentDidMount() {
+//   this.fetchWeather(-10,10).then(res => console.log(res))
+// }
+
+// To allow:
+// edit info.plist
+// below <dict>
+// <key>NSAllowsArbitraryLoads</key>
+// </true>
 
 const rootUrl = 'http://api.weather.com.......'
 
 export const fetchWeather = (lat, lon) => {
   const url = rootUrl + '&lat=' + lat + '&lon=' + lon
   console.log(url)
+
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      temp: json.main.temp,
+      weather: json.weather[0].main
+    })
 }
