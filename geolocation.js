48c2af5426f8f60925be624479c474647db03b7a
Wendelcorp <bryce@brycewendelaar.com>
2017-06-04 20:01:00 -0400

    Added geolocation

---
+++ geolocation.js
@@ -0,0 +1,11 @@
+// componentDidMount() {
+//   this.getLocation()
+// }
+
+getLocation() {
+  navigator.geolocation.getCurrentPosition(
+    (posData) => console.log(posData),
+    (error) => alert(error),
+    {timeout:10000}
+  )
+}
